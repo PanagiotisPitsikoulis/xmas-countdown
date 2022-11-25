4191758729dbd3e71cb6064749978f24e3317e86
PanagiotisPitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2022-11-26 00:09:31 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# xmas-countdown
+# interactive-ui.js
+Multiple interactive UI interfaces built on javascript. 
+Our aim is to build a collection of state-of-the-art interactive and modern looking UI templates free for everyone to use. 
---
+++ index.js
@@ -0,0 +1,25 @@
+//days until Christmas counter
+let count = fetchDayCount();
+document.getElementById("count").innerText = count;
+
+function fetchDayCount() {
+  var dayDiff =
+    new Date("Jan 1, 2023 00:00:00").getTime() - new Date().getTime();
+  console.log(Math.floor(dayDiff / (1000 * 60 * 60 * 24)));
+  return Math.floor(dayDiff / (1000 * 60 * 60 * 24));
+}
+//hide instagram button
+document.getElementById("instagram-button").style.display = "none";
+//like counter
+let likes = 0;
+async function likeAction() {
+  console.log(likes);
+  likes++;
+  document.getElementById("like-counter").innerText = likes;
+  //disable like button
+  document.getElementById("like-button").disabled = true;
+  await new Promise((r) => setTimeout(r, 1000));
+  document.getElementById("like-button").style.display = "none";
+  //add instagram button
+  document.getElementById("instagram-button").style.display = "block";
+}
