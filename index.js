035a111168470fc8710d792066bd3ba87ed59aee
PanagiotisPitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2022-11-26 02:03:56 +0200

    UI overhaul

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ function fetchDayCount() {
   console.log(Math.floor(dayDiff / (1000 * 60 * 60 * 24)));
   return Math.floor(dayDiff / (1000 * 60 * 60 * 24));
 }
-//hide instagram button
-document.getElementById("instagram-button").style.display = "none";
 //like counter
 let likes = 0;
 async function likeAction() {
@@ -20,6 +18,4 @@ async function likeAction() {
   document.getElementById("like-button").disabled = true;
   await new Promise((r) => setTimeout(r, 1000));
   document.getElementById("like-button").style.display = "none";
-  //add instagram button
-  document.getElementById("instagram-button").style.display = "block";
 }
