9673d5e6a64543c6604a9c0c6868c21a54763404
PanagiotisPitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2022-11-29 17:42:56 +0200

    ui cleanup

--- index.js
+++ index.js
@@ -8,14 +8,3 @@ function fetchDayCount() {
   console.log(Math.floor(dayDiff / (1000 * 60 * 60 * 24)));
   return Math.floor(dayDiff / (1000 * 60 * 60 * 24));
 }
-//like counter
-let likes = 0;
-async function likeAction() {
-  console.log(likes);
-  likes++;
-  document.getElementById("like-counter").innerText = likes;
-  //disable like button
-  document.getElementById("like-button").disabled = true;
-  await new Promise((r) => setTimeout(r, 1000));
-  document.getElementById("like-button").style.display = "none";
-}
