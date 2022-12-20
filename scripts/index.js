fc49b24cc095c1fe21e872ce1b1a0dc2f226fb91
PanagiotisPitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2022-12-20 20:36:43 +0200

    fixed socials page

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ let count = fetchDayCount();
 document.getElementById("count").innerText = count;
 
 function fetchDayCount() {
-  var dayDiff =
+  let dayDiff =
     new Date("Jan 1, 2023 00:00:00").getTime() - new Date().getTime();
   console.log(Math.floor(dayDiff / (1000 * 60 * 60 * 24)));
   return Math.floor(dayDiff / (1000 * 60 * 60 * 24));
---
+++ scripts/socials.js
@@ -0,0 +1,6 @@
+document.getElementById("instagram_img").onclick = function() {
+    window.location.href = "https://www.instagram.com/panospitsi";
+};
+document.getElementById("github_img").onclick = function() {
+    window.location.href = "https://github.com/PanagiotisPitsikoulis";
+};
